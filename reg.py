75a6052c82cc38c656ca66cdb4b3e4bb0bc73019
phanit1 <babithota5@msitprogram.net>
2020-04-15 17:16:01 +0530

    Registered Email gets displayed after Registration

--- reg.py
+++ reg.py
@@ -1,12 +1,16 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template , request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>Hello World!</h1>'
-@app.route('/registration')
+    return '<h1>Hello! Please add registration in the above url to register yourselves to website</h1>'
+@app.route('/registration',methods = ['POST', 'GET'])
 def hello():
-    return render_template("reg.html")
+    if request.method == 'POST':
+      data = request.form
+      return render_template("registrationdata.html",registrationdata = data)
+    else:
+        return render_template("reg.html")
 
 if __name__ == '__main__':
    app.run(debug = True)
