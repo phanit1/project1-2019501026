03ce02a53bb1d2092d6bf61e90427cfcc2d7983e
phanit1 <babithota5@msitprogram.net>
2020-04-20 21:57:32 +0530

    Done with importing books.csv to heroku

---
+++ books.py
@@ -0,0 +1,22 @@
+import os, csv
+from booksdb import *
+from sqlalchemy import create_engine, desc
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
+def main():
+    Base.metadata.create_all(bind=engine)
+    f = open("books.csv")
+    # print(f)
+    reader = csv.reader(f)
+    next(reader)
+    for isbn, title, author, year in reader:
+        book = Books(isbn=isbn, title=title, author=author, year=int(year))
+        db.add(book)
+    db.commit()
+    db.close()
+
+if __name__ == "__main__":
+    main()
