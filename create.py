21a286952e27b180e0ce36a9f51ae032b3484d7a
phanit1 <babithota5@msitprogram.net>
2020-04-26 22:12:25 +0530

    Added search feature

--- create.py
+++ create.py
@@ -1,18 +1,22 @@
 import os
-from flask import Flask, request, render_template
+
+from flask import Flask, request, render_template,url_for
 from models import *
+from books import *
 from flask import Flask, session, redirect
 from flask_session import Session
-from sqlalchemy import create_engine, desc
+from sqlalchemy import create_engine, desc , or_
 from sqlalchemy.orm import scoped_session, sessionmaker
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+
 engine = create_engine(os.getenv("DATABASE_URL"))
 
 Session(app)
@@ -20,12 +24,11 @@ db.init_app(app)
 
 @app.route('/')
 def hello_world():
-    return redirect('/register')
-@app.route('/register', methods = ['POST','GET'])
+    return redirect('/registration')
+@app.route('/registration', methods = ['POST','GET'])
 def register():
     db.create_all()
     if request.method == 'POST':
-        data = request.form
         userdata = USERS(request.form['email'],request.form['psw'])
         user = USERS.query.filter_by(emailid=request.form['email']).first()
         if user is not None:
@@ -44,7 +47,6 @@ def register():
 @app.route('/admin')
 def admin():
     data = USERS.query.order_by(desc(USERS.timestamp)).all()
-    print(data)
     return render_template("admin.html",admin = data)
 
 @app.route('/auth', methods=['POST'])
@@ -63,12 +65,21 @@ def login():
         print("You are not a registered user. Please first register to login")
         var1 = "Error: You are not a registered user. Please first register to login"
         return render_template("reg.html", var1 = var1)
-@app.route('/home')
+@app.route('/home', methods=['POST','GET'])
 def home():
     try:
         user=session['email']
-        return render_template("login.html")
-    except:
+        if request.method == 'POST':
+            req  = request.form['search']
+            reqs = str(req)
+            bookss = dbscope.query(Books.isbn, Books.title, Books.author, Books.year).filter(or_(Books.title.like("%"+reqs+"%"), Books.author.like("%"+reqs+"%"), Books.isbn.like("%"+reqs+"%"))).all()
+            if bookss.__len__()==0:
+                var1 = "No search found"
+                return render_template("login.html", var1 = var1, user = user)
+            return render_template("login.html",bookss = bookss,formaction = '/home', user = user)
+        return render_template("login.html", user = user)
+    except Exception as e:
+        print(e)
         var1 = "You must log in to view the homepage"
         return render_template("reg.html",var1 = var1)
 
