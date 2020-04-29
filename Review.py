a64f76598e3a0a399fca1d32c39b42dce8a23e85
swapnith8 <kswapnith@msitprogram.net>
2020-04-29 12:57:19 +0530

    schema changed

--- Review.py
+++ Review.py
@@ -2,15 +2,14 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Review(db.Model):
-    __tablename__ = "review"
-    names = db.Column(db.String,primary_key=True)
+    __tablename__ = "reviewdata"
     isbn = db.Column(db.String, primary_key=True)
     email = db.Column(db.String, primary_key=True)
     comment = db.Column(db.String, nullable=False)
     rating = db.Column(db.String, nullable=False)
     
-    def __init__(self,names,isbn,email,comment,rating) :
-        self.names = names
+    def __init__(self,isbn,email,comment,rating) :
+        
         self.isbn = isbn
         self.email = email
         self.comment=comment
